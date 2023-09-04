632c0042b7190d1a7503e80bbccc31e076accfa3
504 <504@DESKTOP-2TRGUA3>
2023-09-04 17:07:00 +0900

    ㅌㅇ

--- ezenTeam1/src/main/webapp/js/sns.js
+++ ezenTeam1/src/main/webapp/js/sns.js
@@ -16,13 +16,12 @@ function sdelete(){
 	if(dconfirm == true) {
 		let mpwd = prompt('비밀번호 확인');
 		console.log(mpwd);
-	// 3. ajax [ 입력받은 패스워드 전송해서 로그인된 회원(서블릿세션)의 패스워드와 입력받은 패스워드가 일치하면 탈퇴]
 	$.ajax({
-		url : "/jspweb/MemberInfoController",
+		url : "/sns/SnsController",
 		method : "delete",
-		data : { mpwd : mpwd},
+		data : { spwd : spwd},
 		success : r => {
-			if(r){alert('회원탈퇴 했습니다.'); logout();}
+			if(r){alert('삭제 성공.');}
 			else{alert('패스워드가 일치하지 않습니다.');}
 			
 		} , 
