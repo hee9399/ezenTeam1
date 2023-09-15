93a5a3bd86cc5660b36d9ceb650fdbe46f0a578d
박상빈 <hyunsk9861@gmail.com>
2023-09-15 15:26:56 +0900

    15 , 15 : 26 , 박상빈 , 중간 커밋

---
+++ ezenTeam1/src/main/java/controller/admin/AdminController.java
@@ -0,0 +1,39 @@
+package controller.admin;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/AdminController")
+public class AdminController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public AdminController() {
+       
+    }
+
+	// 1. 회원가입 간단한 정보 최신순으로 조회
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
---
+++ ezenTeam1/src/main/java/model/dao/AdminDao.java
@@ -0,0 +1,43 @@
+package model.dao;
+
+import java.util.ArrayList;
+
+import model.dto.RiderDto;
+
+public class AdminDao extends Dao{
+	// 0. 싱글톤
+	private static AdminDao adminDao = new AdminDao();
+	private AdminDao() {}
+	public static AdminDao getInstance() {return adminDao;}
+	
+	
+	
+	// 1. 기본적인 내용 호출
+	public ArrayList<RiderDto> ApprovalPrint(){
+		ArrayList<RiderDto> list = new ArrayList<>();
+		try {
+			// 2.  최신순으로 라이더가 회원가입 요청했을때 간단한 라이더 정보 가져오는 쿼리문
+			String sql ="select rno , rid , rdate from rider order by rdate desc ";
+			ps = conn.prepareStatement(sql);
+			rs=ps.executeQuery();
+			
+			// 3. 정보가 여러개니깐 while문으로 여러개 레코드 조회
+			while(rs.next()) {
+				RiderDto dto = new RiderDto(
+						rs.getInt("rno"), rs.getString("rid"),
+						rs.getString("rdate")	);
+				
+				// 4. 리스트에 dto 를 담는다,
+				list.add(dto);
+			}
+			
+		}catch (Exception e) {System.out.println(e);}
+		// 5. 리스트에 dto 를 담아서 리턴.
+		return list;
+	}
+	
+	
+	// 2. 상세보기 
+	
+	
+}
