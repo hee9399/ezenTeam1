0024ba270db67e237b870a4a25a51b38617ec42f
504 <504@DESKTOP-PI60H60>
2023-09-22 18:02:46 +0900

    0922

--- ezenTeam1/src/main/webapp/js/gorider/member/minfo.js
+++ ezenTeam1/src/main/webapp/js/gorider/member/minfo.js
@@ -1,60 +1,10 @@
 /**
  *
  */
-let loginState = false; //로그인상태 기본값 false로 세팅
-let loginMid = ''; // 전역변수로 선언
-getMemberInfo();
-// 1. 세션에 있는 로그인정보 유무에 따라  화면수정
-function getMemberInfo(){
-	$.ajax({
-		url:"/ezenTeam1/MemberInfoController",
-		method:"get",
-		async:false, //동기화설정(waiting)
-		data:{type:'login'},
-		success: r =>{
-			let btnBox = document.querySelector('.hbtnbox');
-			let html =``;
-
-			//console.log("성공" + r);
-			if(r == null){ //세션정보가 없으면 회원가입/ 로그인 버튼 활성화
-				loginState = false;
-				loginMid = '';
-				html +=`<div onclick="onSignUp()"  class="btn r1">가입 </div>
-				        <div onclick="onLogin()"  class="btn r1"> 로그인 </div> `
-			
-			} else {//세션정보가 있으면 회원명 프로필사진 출력
-				loginState = true;
-				loginMid = r.mid;
-				let mphoto = r.mphoto == null? 'default.png': r.mphoto;
-				html += ` <div class="minfo">
-							<span class="mname">${r.mname}</span>
-							<span class="sayhello"> 반갑습니다.</span> 
-						    <img src="/ezenTeam1/gorider/member/file/${mphoto}"/>
-						   </div>`
-
-			}
-			btnBox.innerHTML = html;
-			console.log("loginState :: "+loginState);
-			console.log("loginMid :: "+loginMid);
-			console.log("loginMname :: "+r.mname);
-		},
-		error : e =>{console.log("실패"+ e)}
-	})
+if(loginState== false){
+	location.href='/ezenTeam1/gorider/member/mlogin.jsp'
+} else {
+//	document.querySelector('.mi_mname').innerHTML = loginMname;
+//	document.querySelector('.mi_memail').innerHTML = loginMeamil;
 }
 
-function logout(){
-	$.ajax({
-		url:"/ezenTeam1/MemberFindController",
-		method:"post",
-		data:{type:"logout"},
-		success: r =>{
-
-			//console.log("성공" + r);
-			if(r){
-				alert('로그아웃 되었습니다.');
-				location.href = '/ezenTeam1/gorider/member/mindex.jsp'
-			}
-		},
-		error : e =>{console.log("실패 :: "+ e)}
-	})
-}
