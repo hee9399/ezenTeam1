a4449a17254ecadc5cab7ecad3ffac494bf927e4
gmlfk <gmlfk@192.168.219.126>
2023-10-03 23:45:39 +0900

    22:45 , 10/03 , 취합

--- ezenTeam1/src/main/webapp/js/gorider/rider/riderSignup.js
+++ ezenTeam1/src/main/webapp/js/gorider/rider/riderSignup.js
@@ -11,7 +11,7 @@ console.log('js실행됩니다.');
  */
 
 // 회원가입  체크여부 
-let checkList = [ false , false ] // 아이디 , 비밀번호 체크여부
+let checkList = [ false , false ]  // 아이디 , 비밀번호 체크여부
 
 
 // 1. 유효성 검사없는 회원가입 Form데이터전송시 
