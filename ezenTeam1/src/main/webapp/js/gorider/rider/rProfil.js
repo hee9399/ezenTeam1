c061c00139f97b9eaeb65c90a62cf681748d4367
504 <504@DESKTOP-FRGIVND>
2023-10-04 18:45:15 +0900

    18:45 , 10/04 , 정희락

--- ezenTeam1/src/main/webapp/js/gorider/rider/rProfil.js
+++ ezenTeam1/src/main/webapp/js/gorider/rider/rProfil.js
@@ -4,20 +4,19 @@ console.log('js실행됩니다.');
 // - 로그인된 라이더 이름과 프로필사진이 등록되야한다. 
 
 // 1. 라이더 식별번호 전역변수 선언 
-let rno = new URL( location.href ).searchParams.get("rno");
-console.log(rno);
 
    $.ajax({
       url : "/ezenTeam1/RiderInfoController",      
-      data : {type: "rphoto" , rno: rno},      
-      method : "put",   
+      data : {type: "info" },      
+      method : "get",   
       success : r => {console.log(r);
       
       		// 1. 해당 로그인된 라이더 프로필사진 이미지 출력
-      		document.querySelector('.rphoto').src=`/ezenTeam1/gorider/rider/img/${r.rphoto}}`;
+      		document.querySelector('.rphoto img').src=`/ezenTeam1/gorider/rider/img/${r.rphoto}`;
       		
       		// 2. 해당 로그인된 라이더 이름 출력 
-      		document.querySelector('.rname').src=`/ezenTeam1/gorider/rider/img/${r.rname}`;
+      		document.querySelector('.rname').value =`${r.rname}`;
+      		
       		
       } ,       
       error :  e => {} ,         
