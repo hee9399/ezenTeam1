05e81900bb166c7f3ab2caa8fb5225eca00297ff
junhee0125 <minervajun@gmail.com>
2023-10-06 14:42:02 +0900

    sql

--- ezenTeam1/src/goRider_사용_sql.sql
+++ ezenTeam1/src/goRider_사용_sql.sql
@@ -12,7 +12,7 @@ create table member(
     memail varchar(50) not null unique,   # 회원이메일
     mpayinfo varchar(16) not null,         # 결제카드번호
     mdate datetime default now(),           # 회원등록일
-    mphoto longtext,                         # 회원프로필
+    mphoto longtext,                         # 회원프로필사진
     primary key( mno )
 );
 
@@ -28,36 +28,35 @@ create table rider(
     rregistration longtext,				# 차량등록증  //이미지
     rdate datetime default now(),		# 등록일
     raccount varchar(20),				# 계좌번호
-     rbank varchar(3) , 				# 라이더 은행명
-    rstatus varchar(1),					# 승인상태
+    rbank varchar(3) , 					# 라이더 은행명
+    rstatus varchar(1),					# 승인상태 Y / N / D
     rcomment text,						# 승인거부시 사유
-    라이더위도 varchar(30),
-	라이더경도 varchar(30),
+    rbikenum varchar(20),				# 바이크넘		
     primary key( rno )
 );
-alter table rider add rbikenum varchar(20);
-alter table rider add 라이더위도 varchar(30)
-alter table rider add 라이더경도 varchar(30)
+
 
 # 	 라이더상태테이블 - 라이더번호 , 라이더출근상태 , 라이더콜가능상태
 drop table if exists riderstate;
 create table riderstate(
 	 rno int ,     			# 라이더번호
-     rstart varchar(2) ,    # 라이더출근상태
-     rcall varchar(2) ,  	# 라이더콜가능상태
+     rstart varchar(2) default 'N'  ,    # 라이더출근상태  Y(출근),  N(퇴근)
+     rcall varchar(2) default 'N' ,  	# 라이더콜가능상태 Y(출근),  N(퇴근)
      foreign key(rno) references rider(rno) on delete cascade on update cascade
 );
 
 drop table if exists service;
 create table service(
-	sno	 int auto_increment,				       # 서비스번호
+	sno	 int auto_increment,			# 서비스번호
 	mno int, 				    		# 회원번호
     rno int  ,     						# 회원번호
     sdate datetime default now(),		# 서비스이용일
-    sfromla varchar(30) ,						# 서비스 시작위치 위도
-    sfromlo varchar(30),						# 서비스 시작위치 경도
-    stola varchar(30),						# 서비스 도착위치 위도
-    stolo varchar(30),						# 서비스 도착위치 경도
+    sriderla varchar(30) ,				# 라이더 출발위치 위도
+    sriderlo varchar(30),				# 라이더 출발위치 경도
+    sfromla varchar(30) ,				# 서비스 시작위치 위도
+    sfromlo varchar(30),				# 서비스 시작위치 경도
+    stola varchar(30),					# 서비스 도착위치 위도
+    stolo varchar(30),					# 서비스 도착위치 경도
     spayment int,						# 서비스결제금액
     spayYN varchar(1),					# 결제여부
     sreview text,						# 리뷰
@@ -68,13 +67,16 @@ create table service(
 );
 
 drop table if exists deposit;
-create table deposit ( # 결제 테이블
+create table deposit ( 				# 입금 테이블
 	dno int auto_increment,			# 입금번호
     rno int  ,     					# 라이더번호
+    sno	 int,
     ddate datetime default now(),	# 입금일
     ddeposit int,					# 입금액
 	primary key( dno ),
     foreign key( rno) references rider(rno) on delete cascade on update cascade
+    foreign key( sno) references service(sno) on delete cascade on update cascade ,
+
 );
 
 # 게시판(공지사항/이벤트)
@@ -161,18 +163,35 @@ delete from rider where rno = 13 and rpwd = 'As13511351';
 
 
 # 라이더 상태 샘플
-insert into riderstate( rno ,  rstart , rcall ) values( 9 , 'y' , 'y' );
-insert into riderstate( rno , rstart , rcall ) values( 12 , 'n' , 'n' );
+insert into riderstate( rno ,  rstart , rcall ) values( 9 , 'Y' , 'Y' );
+insert into riderstate( rno , rstart , rcall ) values( 12 , 'N' , 'N' );
 
 select rno from rider natural join riderstate where rid = 'hee9399';
 
+#service
+insert into service values(1,1,1,'2023-09-27 15:41:38',123.123,123.123,123.123,123.123,1000,'Y','개빡치네','5');
+insert into service values(2,2,2,'2023-09-27 15:41:38',123.123,123.123,123.123,123.123,1000,'Y','개빡치네','5');
+insert into service values(3,3,3,'2023-09-27 15:41:40',123.123,123.123,123.123,123.123,1000,'Y','개빡치네','5');
 
 
 #board 추가/ 삭제
 insert into board (btarget,btype,btitle,bcontent,bstartdate,benddate) values('U','N','ddddddd','aaaaaaa',null,null);
 delete from board where bno = '1';
 select * from board b natural join boardfiles bf order by desc;
-
+#공지 상세
+select 
+		b.bno as bno
+	  , b.btarget as btarget
+	  , b.btype as btype
+      , b.btitle as btitle
+      , b.bcontent as bcontent
+      , DATE_FORMAT(bdate, '%Y-%m-%d') as bdate
+      , b.bview as bview
+      , DATE_FORMAT(b.bstartdate, '%Y-%m-%d') as bstartdate
+      , DATE_FORMAT(b.benddate, '%Y-%m-%d') as benddate
+      , bf.bfno as bfno
+      , bf.bfile as bfile
+ from board b left outer join boardfiles bf on b.bno = bf.bno where b.bno = 2 ;
 
 
 
